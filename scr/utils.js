8b24f7076f3afe8cb7647049705bb30af38a84e9
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-10 19:42:55 +0300

    getting info from local storage implemented

--- scr/utils.js
+++ scr/utils.js
@@ -1,1 +1,9 @@
 export const APIKEY = "mz7BQ5LzMjrvdnaBJoaUCkwMDO8Xb7GX";
+
+export const getUploadsFromStorage = () => {
+    const uploadsFromStorage = localStorage.getItem('uploads');
+    console.log(uploadsFromStorage)
+    const result = uploadsFromStorage ? JSON.parse(uploadsFromStorage) : [];
+
+    return result;
+};
