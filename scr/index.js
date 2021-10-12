dcf4376960e7f10abb5d3975686155de77c1c325
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-12 19:14:36 +0300

    updated eventListeners

--- scr/index.js
+++ scr/index.js
@@ -4,20 +4,29 @@ import {trending} from './trending-api.js';
 import {upload} from './uploadGIF-api.js';
 import {displayUploadedGifs} from './display-uploadedGIFs-api.js';
 import {showDetails} from './details-api.js';
+import {showUpload} from './upload-view.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('click', (event) => {
+    //   event.preventDefault();
     if (event.target.classList.contains('view-details')) {
       showDetails(event.target.id);
-    }
-    if (event.target.id === 'btnSearch') {
-      search();
-    }
-    if (event.target.id === 'btnTrending') {
-      trending();
-    }
-    if (event.target.id === 'btnUploadedGifs') {
-      displayUploadedGifs();
+    } else {
+      switch (event.target.id) {
+        case 'btnSearch':
+          search();
+          break;
+        case 'btnTrending':
+          trending();
+          break;
+        case 'btnUploadedGifs':
+          displayUploadedGifs();
+          break;
+        case 'btnUpload':
+          showUpload();
+          document.querySelector('input[type="file"]').addEventListener('change', upload);
+          break;
+      }
     }
   });
   document.getElementById('search').addEventListener('keyup', (event) => {
@@ -25,6 +34,4 @@ document.addEventListener('DOMContentLoaded', () => {
       search();
     }
   });
-
-  document.querySelector('input[type="file"]').addEventListener('change', upload);
 });
