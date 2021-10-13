ed6fa412945112565ffffbc413f0125c4cacf256
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-13 16:50:10 +0300

    code refactoring to accommodate new views

--- scr/upload/uploadGIF-api.js
+++ scr/upload/uploadGIF-api.js
@@ -1,5 +1,7 @@
 import * as utils from '../utils.js';
-import {showUpload} from '../upload-view.js';
+import {displayProgressBar} from '../progress-bar.js';
+import {showAlertMsg} from '../alert-msg.js';
+import {showSuccessAlertMsg} from '../success-alert-msg.js';
 
 /**
  * Upload a gif from his file system.
@@ -7,25 +9,27 @@ import {showUpload} from '../upload-view.js';
  */
 export const upload = () => {
   const input = document.querySelector('input[type="file"]');
-  const formData = new FormData();
-  formData.append('file', input.files[0]);
-
-  fetch(`https://upload.giphy.com/v1/gifs?api_key=${utils.APIKEY}`, {
-    method: 'POST',
-    body: formData,
-  })
-      .then((response) => response.json())
-      .then((result) => {
-        const uploadedGIFs = utils.getUploadsFromStorage();
-        uploadedGIFs.push(result.data.id);
-        localStorage.setItem('uploads', JSON.stringify(uploadedGIFs));
-        console.log('Success:', result);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      })
-      .finally(() => {
-        // input.value = ''
-        showUpload();
-      });
+  const file = input.files[0];
+  if (file.type !== 'image/gif') {
+    showAlertMsg();
+  } else {
+    const formData = new FormData();
+    formData.append('file', file);
+    displayProgressBar();
+    fetch(`https://upload.giphy.com/v1/gifs?api_key=${utils.APIKEY}`, {
+      method: 'POST',
+      body: formData,
+    })
+        .then((response) => response.json())
+        .then((result) => {
+          const uploadedGIFs = utils.getUploadsFromStorage();
+          uploadedGIFs.push(result.data.id);
+          localStorage.setItem('uploads', JSON.stringify(uploadedGIFs));
+          showSuccessAlertMsg();
+          console.log('Success:', result);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+  }
 };
